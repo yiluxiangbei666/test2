0903355b6850d870b5a477bf31e3d561d2c6bde3
18225310924 <1182764581@qq.com>
2021-04-22 14:17:33 +0800

    adada

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,7 +1,5 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("aa");
-        System.out.println("asdada");
-        System.out.println("sdadasda");
+       
     }
 }
